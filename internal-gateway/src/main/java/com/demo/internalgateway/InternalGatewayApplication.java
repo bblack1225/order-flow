2b50ff5729005b26e67c244117125d8ffeaa18e8
bblack1225 <black60137@gmail.com>
2024-09-02 18:02:12 +0800

    feat: 新增 module- internal gateway,  目前只有基礎的設定

---
+++ internal-gateway/src/main/java/com/demo/internalgateway/InternalGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.demo.internalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternalGatewayApplication.class, args);
+    }
+
+}
