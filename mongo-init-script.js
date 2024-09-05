5e0b5e5899846687bed415614329696e52d16352
bblack1225 <black60137@gmail.com>
2024-09-05 17:55:54 +0800

    feat: 新增mysql 跟 mongodb 的初始資料，在容器初始啟動時可以建立

---
+++ mongo-init-script.js
@@ -0,0 +1,80 @@
+db = db.getSiblingDB("admin");
+db.auth("root", "root");
+
+db = db.getSiblingDB("inventory_db");
+
+db.inventory.insertMany(
+    [
+        {
+            "_id": ObjectId("66d6797e3b8b2b437c89f37a"),
+            "quantity": 20,
+            "status": "available",
+            "lastUpdated": ISODate("2024-09-02T12:00:00.000Z"),
+            "productId": ObjectId("66d675ab3b8b2b437c89f369")
+        },
+        {
+            "_id": ObjectId("66d679c33b8b2b437c89f37f"),
+            "quantity": 20,
+            "status": "available",
+            "lastUpdated": ISODate("2024-09-02T12:00:00.000Z"),
+            "productId": ObjectId("66d6766e3b8b2b437c89f36d")
+        },
+        {
+            "_id": ObjectId("66d679f63b8b2b437c89f382"),
+            "quantity": 20,
+            "status": "available",
+            "lastUpdated": ISODate("2024-09-02T12:00:00.000Z"),
+            "productId": ObjectId("66d676893b8b2b437c89f36e")
+        },
+        {
+            "_id": ObjectId("66d67a103b8b2b437c89f383"),
+            "quantity": 20,
+            "status": "available",
+            "lastUpdated": ISODate("2024-09-02T12:00:00.000Z"),
+            "productId": ObjectId("66d676943b8b2b437c89f36f")
+        }
+    ]
+);
+
+db.products.insertMany(
+    [
+        {
+            "_id": ObjectId("66d675ab3b8b2b437c89f369"),
+            "name": "iPhone11",
+            "description": "iPhone11",
+            "category": "Cellphone",
+            "price": 30000,
+            "createAt": ISODate("2024-09-02T12:00:00.000Z"),
+            "updatedAt": ISODate("2024-09-02T12:00:00.000Z")
+        },
+        {
+            "_id": ObjectId("66d6766e3b8b2b437c89f36d"),
+            "name": "iPhone12",
+            "description": "iPhone12",
+            "category": "Cellphone",
+            "price": 31000,
+            "createAt": ISODate("2024-09-02T12:00:00.000Z"),
+            "updatedAt": ISODate("2024-09-02T12:00:00.000Z")
+        },
+        {
+            "_id": ObjectId("66d676893b8b2b437c89f36e"),
+            "name": "iPhone13",
+            "description": "iPhone13",
+            "category": "Cellphone",
+            "price": 33000,
+            "createAt": ISODate("2024-09-02T12:00:00.000Z"),
+            "updatedAt": ISODate("2024-09-02T12:00:00.000Z")
+        },
+        {
+            "_id": ObjectId("66d676943b8b2b437c89f36f"),
+            "name": "iPhone14",
+            "description": "iPhone14",
+            "category": "Cellphone",
+            "price": 35000,
+            "createAt": ISODate("2024-09-02T12:00:00.000Z"),
+            "updatedAt": ISODate("2024-09-02T12:00:00.000Z")
+        }
+    ]
+);
+
+db.createCollection("inventory_history");
---
+++ mysql-init.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS orders;
+CREATE USER 'guest'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON mydb.* TO 'guest'@'%';
+FLUSH PRIVILEGES;
+
+CREATE TABLE orders.order_information (
+                                          id INT auto_increment PRIMARY KEY,
+                                          product_id NVARCHAR(255) NOT NULL,
+                                          name NVARCHAR(255) NOT NULL,
+                                          order_qty INT NOT NULL,
+                                          actual_qty INT NOT NULL,
+                                          price INT NOT NULL,
+                                          payer_name NVARCHAR(255) NOT NULL,
+                                          phone NVARCHAR(50),
+                                          email NVARCHAR(255),
+                                          address NVARCHAR(255),
+                                          payment_name NVARCHAR(100),
+                                          payment_status INT,
+                                          description NVARCHAR(255),
+                                          status INT NOT NULL
+);
